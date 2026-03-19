16b8c87818b6dfcd17edd418a39ba167b819dfbc
Varshitha Besthavemula <varshithabesthavemula@gmail.com>
2026-03-19 22:28:17 +0530

    add public urls in config

--- apps/excalidraw-frontend/config.ts
+++ apps/excalidraw-frontend/config.ts
@@ -1,6 +1,6 @@
-export const HTTP_BACKEND = "http://localhost:3001";
-// export const HTTP_BACKEND = "https://excalidraw-2-00rd.onrender.com";
+// export const HTTP_BACKEND = "http://localhost:3001";
+export const HTTP_BACKEND = "https://excalidraw-2-00rd.onrender.com";
 
-// export const WS_URl = "https://excalidraw-ws-backend.onrender.com";
-export const WS_URl = "ws://localhost:8080";
+export const WS_URl = "https://excalidraw-ws-backend.onrender.com";
+// export const WS_URl = "ws://localhost:8080";
 // const token = localStorage.getItem("token");
